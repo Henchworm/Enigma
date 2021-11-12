dd7cf38f00e012b7f6f3ac2bcf6455edff840e29
Chris Hewitt <agop5134@gmail.com>
2021-11-11 22:43:39 -0700

    Rotation variable removal refactor

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,14 @@
 require 'date'
 class Enigma
-  attr_reader :alphabet
+  attr_reader :alphabet,
+              :encrypted
   def initialize
     @alphabet = ("a".."z").to_a << " "
   end
 
   def key_generator
    rand(99999).to_s.rjust(5, '0')
- end
+  end
 
   def offset_generator(date = Date.today)
     if date.class == Date
@@ -30,32 +31,27 @@ class Enigma
 
   def encrypt(message, key = key_generator, date = offset_generator)
     date = offset_generator(date)
-    encrypted = []
     shift_hash = shift(key, date)
+    encrypted = []
     message = message.downcase.strip.split("")
     message.each_with_index do |character, index|
       if index % 4 == 0
-        char_index = @alphabet.index(character)
-        rotation_1 = @alphabet.rotate(char_index)
+        rotation_1 = @alphabet.rotate(alphabet.index(character))
         rotation_2 = rotation_1.rotate(shift_hash['A'])
           encrypted << rotation_2[0]
       elsif index % 4 == 1
-        char_index = @alphabet.index(character)
-        rotation_1 = @alphabet.rotate(char_index)
+        rotation_1 = @alphabet.rotate(alphabet.index(character))
         rotation_2 = rotation_1.rotate(shift_hash['B'])
           encrypted << rotation_2[0]
       elsif index % 4 == 2
-        char_index = @alphabet.index(character)
-        rotation_1 = @alphabet.rotate(char_index)
+        rotation_1 = @alphabet.rotate(alphabet.index(character))
         rotation_2 = rotation_1.rotate(shift_hash['C'])
           encrypted << rotation_2[0]
       elsif index % 4 == 3
-        char_index = @alphabet.index(character)
-        rotation_1 = @alphabet.rotate(char_index)
+        rotation_1 = @alphabet.rotate(alphabet.index(character))
         rotation_2 = rotation_1.rotate(shift_hash['D'])
           encrypted << rotation_2[0]
       end
-      #1st rotations can be broken into helper methods?
     end
     encryption_hash = {
        'encryption': encrypted.join,
