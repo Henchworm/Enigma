672e936932813ecd737eb8db4f076921740c7a72
Chris Hewitt <agop5134@gmail.com>
2021-11-15 10:04:46 -0700

    Move formatting/specials methods to module

--- lib/Generator.rb
+++ lib/Generator.rb
@@ -12,4 +12,14 @@ module Generator
      (date.to_i ** 2).to_s.split("").last(4)
     end.join
   end
+
+  def shift(randkey = key_generator, offset = offset_generator)
+    key_pairs = randkey.split("").map(&:to_i).each_cons(2).map {|a| a.join.to_i}
+    shift_hash = {
+      "A" => (key_pairs[0] + offset[0].to_i),
+      "B" => (key_pairs[1] + offset[1].to_i),
+      "C" => (key_pairs[2] + offset[2].to_i),
+      "D" => (key_pairs[3] + offset[3].to_i)
+    }
+  end
 end
--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,44 +1,13 @@
 require 'date'
-require_relative './Generator'
+require_relative './generator'
+require_relative './formatting'
 class Enigma
   include Generator
+  include Formatting
   attr_reader :alphabet,
               :encrypted
   def initialize
     @alphabet = ("a".."z").to_a << " "
-  end
-
-  def shift(randkey = key_generator, offset = offset_generator)
-    key_pairs = randkey.split("").map(&:to_i).each_cons(2).map {|a| a.join.to_i}
-    shift_hash = {
-      "A" => (key_pairs[0] + offset[0].to_i),
-      "B" => (key_pairs[1] + offset[1].to_i),
-      "C" => (key_pairs[2] + offset[2].to_i),
-      "D" => (key_pairs[3] + offset[3].to_i)
-    }
-  end
-
-  def pop_specials(message)
-    message.downcase.split('').find_all do |character|
-      @alphabet.include?(character)
-    end
-  end
-
-  def specials_arrays(message)
-    specials = []
-    message.downcase.split('').each_with_index do |character, index|
-      if !@alphabet.include?(character)
-        specials << (character + index.to_s).split
-      end
-    end
-    specials
-  end
-
-  def insert_specials(encrypted, message)
-    specials_arrays(message).each do |char|
-      encrypted.insert(char[0][1..-1].to_i, char[0][0])
-    end
-    encrypted.join
   end
 
   def first_rotation(character)
@@ -63,7 +32,6 @@ class Enigma
     insert_specials(encrypted, message) && hash_return_encrypt(encrypted,key,date)
   end
 
-
   def decrypt(message, key = key_generator, date = Date.today)
     offset = offset_generator(date)
     shift_hash = shift(key, offset)
@@ -80,29 +48,5 @@ class Enigma
         end
     end
     insert_specials(decrypted, message) && hash_return_decrypt(decrypted,key,date)
-  end
-
-  def hash_return_encrypt(encrypted,key,date)
-    encryption_hash = {
-       'encryption': encrypted.join,
-       'key': key,
-       'date': date_formatter(date)
-     }
-  end
-
-  def hash_return_decrypt(decrypted,key,date)
-    encryption_hash = {
-       'decryption': decrypted.join,
-       'key': key,
-       'date': date_formatter(date)
-     }
-  end
-
-  def date_formatter(date)
-    if date.class == Date
-     formatted = date.strftime("%d,%m,%y").gsub(/,/, '')
-    else
-      date
-    end
   end
 end
